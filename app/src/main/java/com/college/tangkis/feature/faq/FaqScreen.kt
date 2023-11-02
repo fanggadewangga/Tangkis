b2140538b9a5dd5beb6d35f52b47a5da440be3c4
fanggadewangga <fanggadewangga@gmail.com>
2023-11-02 13:27:04 +0700

    feat: add bottom nav bar on faq screen

--- app/src/main/java/com/college/tangkis/feature/faq/FaqScreen.kt
+++ app/src/main/java/com/college/tangkis/feature/faq/FaqScreen.kt
@@ -3,34 +3,35 @@ package com.college.tangkis.feature.faq
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.FabPosition
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.college.tangkis.R
-import com.college.tangkis.feature.main.components.AppButton
 import com.college.tangkis.feature.main.components.AppText
-import com.college.tangkis.feature.main.components.EmergencyContactItem
-import com.college.tangkis.feature.main.components.ErrorLayout
 import com.college.tangkis.feature.main.components.FaqItem
+import com.college.tangkis.feature.main.navigation.BottomNavigationBar
+import com.college.tangkis.feature.main.route.Screen
 import com.college.tangkis.theme.Typography
 import com.college.tangkis.theme.md_theme_light_primary
+import com.college.tangkis.theme.md_theme_light_secondary
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -61,12 +62,42 @@ fun FaqScreen(navController: NavController) {
                 }
             )
         },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navController.navigate(Screen.SOS.route) },
+                shape = CircleShape,
+                containerColor = md_theme_light_secondary,
+                modifier = Modifier.size(80.dp)
+            ) {
+                AsyncImage(
+                    model = R.drawable.ic_sos,
+                    contentDescription = "SOS",
+                    modifier = Modifier.size(42.dp)
+                )
+            }
+        },
+        bottomBar = {
+            BottomAppBar(
+                tonalElevation = 8.dp,
+                containerColor = Color.White,
+            ) {
+                BottomNavigationBar(navController = navController)
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = true,
     ) {
         val topPadding = it.calculateTopPadding()
+        val bottomPadding = it.calculateBottomPadding()
 
-        Column (
+        Column(
             modifier = Modifier
-                .padding(top = topPadding + 16.dp, start = 16.dp, end = 16.dp),
+                .padding(
+                    top = topPadding + 16.dp,
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = bottomPadding + 8.dp
+                ),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             // Tulis pertanyaan dan jawaban FAQ
@@ -92,6 +123,6 @@ fun FaqScreen(navController: NavController) {
 
 @Composable
 @Preview
-fun FaqScreenPreview(){
+fun FaqScreenPreview() {
     FaqScreen(navController = rememberNavController())
 }
